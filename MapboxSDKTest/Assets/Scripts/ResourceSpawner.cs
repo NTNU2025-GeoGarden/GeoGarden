7dd427474fe36b02682c08027a6619299bc2f4f6
Ola Horg Jacobsen <olah.jac@gmail.com>
2025-01-14 13:16:22 +0100

    Add basic handling of LatLong and convert to world pos

--- MapboxSDKTest/Assets/Scripts/ResourceSpawner.cs
+++ MapboxSDKTest/Assets/Scripts/ResourceSpawner.cs
@@ -1,13 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Mapbox.BaseModule.Data.Vector2d;
+using Mapbox.BaseModule.Map;
+using Mapbox.Example.Scripts.Map;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ResourceSpawner : MonoBehaviour
 {
-    // Update is called once per frame
+    public GameObject mapObject;
+    public GameObject resourcePrefab;
+    public List<LatitudeLongitude> resourceLocations;
+
+    private MapboxMapBehaviour _map;
+
+    void Start()
+    {
+        _map = mapObject.GetComponent<MapboxMapBehaviour>();
+    }
+    
+        // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            print(Resource.Seed);
+            int index = Random.Range(0, resourceLocations.Count);
+            Instantiate(resourcePrefab, _map.MapInformation.ConvertLatLngToPosition(resourceLocations[index]),
+                Quaternion.identity);
         }
     }
 }

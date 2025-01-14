9ace7b3dd4ac56546762dc6fc8a1cafa6181e863
Ola Horg Jacobsen <olah.jac@gmail.com>
2025-01-14 13:04:06 +0100

    Create resource component

--- MapboxSDKTest/Assets/Scripts/ResourceSpawner.cs
+++ MapboxSDKTest/Assets/Scripts/ResourceSpawner.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class ResourceSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print(Resource.Seed);
+        }
     }
 }

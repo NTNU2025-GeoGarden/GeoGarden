a7d1f00c0d3d0edd05764fca4e297fb5d56f3b37
Ola Horg Jacobsen <olah.jac@gmail.com>
2025-01-21 12:06:53 +0100

    Panning camera and boundary conditions

--- MapboxSDKTest/Assets/Scripts/HomeCameraRotation.cs
+++ MapboxSDKTest/Assets/Scripts/HomeCameraRotation.cs
@@ -10,6 +10,7 @@ public class HomeCameraRotation : MonoBehaviour
     
     private Vector2 _previousPosition;
     private double _previousPinchDistance;
+    private const float SCALE_FACTOR = 0.0001f;
 
     private void Update()
     {
@@ -27,9 +28,36 @@ public class HomeCameraRotation : MonoBehaviour
                 {
                     Vector2 currentPosition = touch.position;
                     Vector2 delta = currentPosition - _previousPosition;
-                    transform.Rotate(Vector3.up, delta.x * speed);
-                    transform.Rotate(Vector3.left, delta.y * speed);
-                    transform.Rotate(0, 0, -transform.rotation.eulerAngles.z);
+                    
+                    transform.Translate(new Vector3((delta.y + delta.x) * speed * SCALE_FACTOR, 0f, (delta.y - delta.x) * speed * SCALE_FACTOR));
+
+                    switch (transform.position.x)
+                    {
+                        case > 3f:
+                            transform.Translate(new Vector3(3f - transform.position.x, 0f, 0f));
+                            break;
+                        case < -0.25f:
+                            transform.Translate(new Vector3(-0.25f - transform.position.x, 0f, 0f));
+                            break;
+                    }
+                    
+                    switch (transform.position.z)
+                    {
+                        case > 2.6f:
+                            transform.Translate(new Vector3(0f, 0f, 2.6f - transform.position.z));
+                            break;
+                        case < -0.6f:
+                            transform.Translate(new Vector3(0f, 0f, -0.6f - transform.position.z));
+                            break;
+                    }
+                        
+                    
+                        
+                        
+
+                    //transform.Rotate(Vector3.up, delta.x * speed);
+                    //transform.Rotate(Vector3.left, delta.y * speed);
+                    //transform.Rotate(0, 0, -transform.rotation.eulerAngles.z);
                     _previousPosition = currentPosition;
                 }
                     

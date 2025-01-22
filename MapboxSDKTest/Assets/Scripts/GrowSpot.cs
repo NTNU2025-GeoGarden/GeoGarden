ad5584755a691dd0778ee8a69c8e7d5cfa295181
Ola Horg Jacobsen <olah.jac@gmail.com>
2025-01-22 13:17:29 +0100

    Added basic plant model, growing stages and plant growing script

---
+++ MapboxSDKTest/Assets/Scripts/GrowSpot.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GrowSpot : MonoBehaviour
+{
+    public GameObject perimiter;
+    
+    public GameObject statusSymbolAddPlant;
+    public GameObject statusSymbolNeedsWaterStage1;
+    public GameObject statusSymbolNeedsWaterStage2;
+    public GameObject statusSymbolNeedsWaterStage3;
+    
+    public GameObject growingStage1;
+    public GameObject growingStage2;
+    public GameObject growingStage3;
+    public GameObject growingStage4;
+}
--- MapboxSDKTest/Assets/Scripts/HomeCameraRotation.cs
+++ MapboxSDKTest/Assets/Scripts/HomeCameraRotation.cs
@@ -11,70 +11,79 @@ public class HomeCameraRotation : MonoBehaviour
     
     private Vector2 _previousPosition;
     private double _previousPinchDistance;
-    private const float SCALE_FACTOR = 0.0001f;
+    private bool _tapped;
 
+    private const float SCALE_FACTOR = 0.0001f;
+    
     private void Update()
     {
         if (Input.touchCount > 0)
         {
-            if (Input.touchCount != 1) return;
-            Touch touch = Input.GetTouch(0);
-            
-            switch (touch.phase)
+            if (Input.touchCount != 1)
             {
-                case TouchPhase.Began:
-                    _previousPosition = touch.position;
-                    break;
-                case TouchPhase.Moved:
+                
+            }
+            else
+            {
+                Touch touch = Input.GetTouch(0);
+            
+                switch (touch.phase)
                 {
-                    Vector2 currentPosition = touch.position;
-                    Vector2 delta = currentPosition - _previousPosition;
+                    case TouchPhase.Began:
+                        _previousPosition = touch.position;
+                        break;
+                    case TouchPhase.Moved:
+                        Vector2 currentPosition = touch.position;
+                        Vector2 delta = currentPosition - _previousPosition;
                     
-                    transform.Translate(new Vector3((delta.y + delta.x) * speed * SCALE_FACTOR, 0f, (delta.y - delta.x) * speed * SCALE_FACTOR));
+                        transform.Translate(new Vector3((delta.y + delta.x) * speed * SCALE_FACTOR, 0f, (delta.y - delta.x) * speed * SCALE_FACTOR));
 
-                    switch (transform.position.x)
-                    {
-                        case > 3f:
-                            transform.Translate(new Vector3(3f - transform.position.x, 0f, 0f));
-                            break;
-                        case < -0.25f:
-                            transform.Translate(new Vector3(-0.25f - transform.position.x, 0f, 0f));
-                            break;
-                    }
+                        switch (transform.position.x)
+                        {
+                            case > 3f:
+                                transform.Translate(new Vector3(3f - transform.position.x, 0f, 0f));
+                                break;
+                            case < -0.25f:
+                                transform.Translate(new Vector3(-0.25f - transform.position.x, 0f, 0f));
+                                break;
+                        }
                     
-                    switch (transform.position.z)
-                    {
-                        case > 2.6f:
-                            transform.Translate(new Vector3(0f, 0f, 2.6f - transform.position.z));
-                            break;
-                        case < -0.6f:
-                            transform.Translate(new Vector3(0f, 0f, -0.6f - transform.position.z));
-                            break;
-                    }
+                        switch (transform.position.z)
+                        {
+                            case > 2.6f:
+                                transform.Translate(new Vector3(0f, 0f, 2.6f - transform.position.z));
+                                break;
+                            case < -0.6f:
+                                transform.Translate(new Vector3(0f, 0f, -0.6f - transform.position.z));
+                                break;
+                        }
                     
-                    _previousPosition = currentPosition;
-                    break;
+                        _previousPosition = currentPosition;
+                        break;
+                    case TouchPhase.Stationary:
+                        if (!_tapped)
+                        {
+                            Ray ray = Camera.main!.ScreenPointToRay(touch.position);
+                            if (Physics.Raycast(ray, out RaycastHit hit, 100))
+                            {
+                                if (hit.transform.CompareTag("PlantSpot"))
+                                {
+                                    GrowSpot spot = hit.transform.GetComponent<GrowSpot>();
+                                    
+                                }
+                            }
+                            
+                            _tapped = true;
+                        }
+                        break;
+                    case TouchPhase.Ended:
+                        _tapped = false;
+                        break;
+                    default:
+                    case TouchPhase.Canceled:
+                        break;
                 }
-                default:
-                case TouchPhase.Stationary:
-                case TouchPhase.Ended:
-                case TouchPhase.Canceled:
-                    break;
             }
-        }
-        else
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                _previousPosition = Input.mousePosition;
-            }
-
-            if (!Input.GetMouseButton(0)) return;
-            
-            Vector2 currentPosition = Input.mousePosition;
-            Vector2 delta = currentPosition - _previousPosition;
-            transform.Rotate(Vector3.up, delta.x * speed);
-            _previousPosition = currentPosition;
         }
     }
 }

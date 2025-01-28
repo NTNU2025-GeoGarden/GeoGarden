d78817261608ad175ef5c2da509bc90539b63b59
Ola Horg Jacobsen <olahja@stud.ntnu.no>
2025-01-28 13:33:52 +0100

    Some initial objects and their icons

---
+++ MapboxSDKTest/Assets/Scripts/Structs/PlaceableObjectType.cs
@@ -0,0 +1,13 @@
+namespace Structs
+{
+    public enum PlaceableObjectType
+    {
+        Tree,
+        Fence,
+        Spot,
+        Lantern,
+        Banner,
+        Cart,
+        WoodPile,
+    }
+}
--- MapboxSDKTest/Assets/Scripts/UI/PlaceObjectUI.cs
+++ MapboxSDKTest/Assets/Scripts/UI/PlaceObjectUI.cs
@@ -1,16 +1,50 @@
+using System;
+using Garden;
+using Structs;
 using UnityEngine;
 
-public class PlaceItemUI : MonoBehaviour
+namespace UI
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlaceItemUI : MonoBehaviour
     {
+        public GameObject treeHolder;
+        public GameObject fenceHolder;
+        public GameObject spotHolder;
+        public GameObject lanternHolder;
+        public GameObject bannerHolder;
+        public GameObject cartHolder;
+        public GameObject woodPileHolder;
         
-    }
+        public PlaceableObjectType type;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void Start()
+        {
+            switch(type)
+            {
+                case PlaceableObjectType.Tree:
+                    treeHolder.SetActive(true);
+                    break;
+                case PlaceableObjectType.Fence:
+                    fenceHolder.SetActive(true);
+                    break;
+                case PlaceableObjectType.Spot:
+                    spotHolder.SetActive(true);
+                    break;
+                case PlaceableObjectType.Lantern:
+                    lanternHolder.SetActive(true);
+                    break;
+                case PlaceableObjectType.Banner:
+                    bannerHolder.SetActive(true);
+                    break;
+                case PlaceableObjectType.Cart:
+                    cartHolder.SetActive(true);
+                    break;
+                case PlaceableObjectType.WoodPile:
+                    woodPileHolder.SetActive(true);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
     }
 }

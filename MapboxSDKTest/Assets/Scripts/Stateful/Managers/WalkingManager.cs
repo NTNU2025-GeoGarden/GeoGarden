fafe9b9361ddb88306547956f564e58ac5dd800b
Haakon Kanter <haakon.kanter@gmail.com>
2025-02-18 12:43:00 +0100

    player now gets energy from walking 100 meters, per now 10 energy

--- MapboxSDKTest/Assets/Scripts/Stateful/Managers/WalkingManager.cs
+++ MapboxSDKTest/Assets/Scripts/Stateful/Managers/WalkingManager.cs
@@ -13,7 +13,10 @@ namespace Stateful.Managers
         private bool _isReady;
         private bool _loaded;
         private LatitudeLongitude _lastPosition;
-        private double _totalDistance; 
+        private double _totalDistance;
+        private double rewardDistanceTracker; // Track distance for rewards
+
+        private double rewardDistance = 0.1; // Track distance for rewards
 
         void Start()
         {
@@ -28,12 +31,24 @@ namespace Stateful.Managers
         {
             if (!(_isReady && _loaded)) return;
             LatitudeLongitude newPosition = map.MapInformation.ConvertPositionToLatLng(playerPosition.position);
-
+            // rewardDistanceTracker is now a class-level variable
+            
+            int energyReward = 10;
+            rewardDistanceTracker += Distance(_lastPosition, newPosition);
             _totalDistance += Distance(_lastPosition, newPosition);
 
             _lastPosition = newPosition;
 
             GameStateManager.CurrentState.DistanceWalked = (float)Math.Round(_totalDistance, 1, MidpointRounding.ToEven);
+
+        
+            if (rewardDistanceTracker >= rewardDistance)
+            {
+                GameStateManager.CurrentState.Energy += energyReward;
+                rewardDistanceTracker = 0; // Reset reward distance tracker after awarding energy
+            }
+ 
+            
         }
 
         /// <summary>
